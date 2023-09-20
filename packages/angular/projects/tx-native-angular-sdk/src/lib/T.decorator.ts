b93b014ba415afa21b029b3465e5dfa517e966a2
Tse018 <60438717+tse018@users.noreply.github.com>
2023-09-20 12:10:08 +0200

    update T.decorator

--- packages/angular/projects/tx-native-angular-sdk/src/lib/T.decorator.ts
+++ packages/angular/projects/tx-native-angular-sdk/src/lib/T.decorator.ts
@@ -1,30 +1,24 @@
 import { Injector } from '@angular/core';
 
-import { ITXInstanceConfiguration } from './interfaces';
 import { TranslationService } from './translation.service';
 
 /**
  * Decorator for using transparently the translation service as a property
  */
-export const T = (str: string, params?: Record<string, unknown>,
-  instanceConfig?: ITXInstanceConfiguration) => (target: any, key: string) => {
-  const injector = Injector.create(
-    {
-      providers: [
-        { provide: TranslationService, useClass: TranslationService },
-      ],
-    },
-  );
+export const T = (
+    str: string,
+    params?: Record<string, unknown>,
+    instanceAlias?: string,
+) => (target: object, key: string) => {
+  const injector = Injector.create({
+    providers: [ { provide: TranslationService, useClass: TranslationService } ],
+  });
   const translationService = injector.get(TranslationService);
 
   Object.defineProperty(target, key, {
     configurable: false,
     get: () => {
-      if (instanceConfig) {
-        translationService.addInstance(instanceConfig);
-      }
-      return translationService.translate(str, { ...params },
-        instanceConfig && instanceConfig.alias || '');
+      return translationService.translate(str, { ...params }, instanceAlias);
     },
   });
 };
